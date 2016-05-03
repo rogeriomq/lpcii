00802e6b677961d025335123d4b0df059ebca5c6
rogeriomq <rogerio.mq@gmail.com>
2016-05-02 22:32:22 -0300

    FUNCAO GENERICA PARA INSERIR OU UPDATE PARA UTILIZAR EM OUTROS BANCOS.

--- Projeto3JDBC/src/main/java/br/edu/unirg/projeto/conexao/dao/RegistroDAO.java
+++ Projeto3JDBC/src/main/java/br/edu/unirg/projeto/conexao/dao/RegistroDAO.java
@@ -13,16 +13,18 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RegistroDAO {
-     ConexaoDB conexao;
+
+    ConexaoDB conexao;
+
     public List<Registro> findAllRegistros() {
         conexao = new ConexaoDB();
         List<Registro> lista = new ArrayList<>();
         String sql = "SELECT * FROM REGISTRO R ORDER BY R.NOME";
         try {
             ResultSet rs = conexao.connect().createStatement().executeQuery(sql);
-            while(rs.next()) {
+            while (rs.next()) {
                 Registro reg = new Registro();
-                reg.setId( rs.getString("ID") );
+                reg.setId(rs.getString("ID"));
                 reg.setNome(rs.getString("NOME"));
                 reg.setSobrenome(rs.getString("SOBRENOME"));
                 reg.setAniversario(rs.getDate("ANIVERSARIO"));
@@ -35,34 +37,34 @@ public class RegistroDAO {
         }
         return lista;
     }
-   
-     public String saveOrUpdate(Registro p) {
+
+    public String saveOrUpdate(Registro p) { // COM ON DUPLICATE KEY, APENAS NO MYSQL E NO MARIADB
         conexao = new ConexaoDB();
         String camposNaoChave = "NOME = ?, SOBRENOME = ?, APELIDO = ?, ANIVERSARIO = ?, ENDERECO = ?";
-        
-        String sqlInsertOrUpdate = "INSERT INTO REGISTRO SET ID = ?,"+ camposNaoChave + " ON DUPLICATE KEY UPDATE " + camposNaoChave;
+
+        String sqlInsertOrUpdate = "INSERT INTO REGISTRO SET ID = ?," + camposNaoChave + " ON DUPLICATE KEY UPDATE " + camposNaoChave;
         //tanto no insert quanto no update, vai ser retornado o ID, caso o ID seja nulo, é pq o SQL nao foi realmente executado.
-        String pkReturn = null; 
+        String pkReturn = null;
         try {
             PreparedStatement ps = conexao.connect()
-                    .prepareStatement(sqlInsertOrUpdate, 
-                        Statement.RETURN_GENERATED_KEYS);
+                    .prepareStatement(sqlInsertOrUpdate,
+                            Statement.RETURN_GENERATED_KEYS);
             System.out.println("ID REGISTRO = " + p.getId());
             //insert Caso o ID seja null, então passo um randomUUID() para o novo contato. 
             //Operador ternário aqui é uma ótima alternativa.
-            ps.setString(1, p.getId()== null ? UUID.randomUUID().toString() : p.getId());//Id somente no insert
+            ps.setString(1, p.getId() == null ? UUID.randomUUID().toString() : p.getId());//Id somente no insert
             ps.setString(2, p.getNome());
             ps.setString(3, p.getSobrenome());
             ps.setString(4, p.getApelido());
-            ps.setDate(5, (p.getAniversario()!= null ? new java.sql.Date(p.getAniversario().getTime()) : null ));
+            ps.setDate(5, (p.getAniversario() != null ? new java.sql.Date(p.getAniversario().getTime()) : null));
             ps.setString(6, p.getEndereco());
             //update
             ps.setString(7, p.getNome());
             ps.setString(8, p.getSobrenome());
             ps.setString(9, p.getApelido());
-            ps.setDate(10, (p.getAniversario()!= null ? new java.sql.Date(p.getAniversario().getTime()) : null ));
+            ps.setDate(10, (p.getAniversario() != null ? new java.sql.Date(p.getAniversario().getTime()) : null));
             ps.setString(11, p.getEndereco());
-            
+
             System.out.println("SQL= " + ps.toString());
             int result = ps.executeUpdate();
             System.out.println("RESULT EXEC SQL= " + result);
@@ -72,5 +74,53 @@ public class RegistroDAO {
         }
         return pkReturn;
     }
-    
+
+    public String saveOrUpdateGenerico(Registro p) {
+        conexao = new ConexaoDB();
+        String camposNaoChave = "NOME = ?, SOBRENOME = ?, APELIDO = ?, ANIVERSARIO = ?, ENDERECO = ?";
+        String sqlInsert = "INSERT INTO REGISTRO SET ID = ?," + camposNaoChave;
+        String sqlUpdate = "UPDATE REGISTRO SET " + camposNaoChave + " WHERE ID=?";
+        //tanto no insert quanto no update, vai ser retornado o ID, caso o ID seja nulo, é pq o SQL nao foi realmente executado.
+        String pkReturn = null;
+        try {
+            ResultSet rs = conexao.connect().createStatement().executeQuery("SELECT COUNT(*) FROM REGISTRO R WHERE R.ID = " + p.getId());
+            PreparedStatement ps;
+            if (rs.getFetchSize() == 0) { //INSERT
+                ps = conexao.connect()
+                        .prepareStatement(sqlInsert,
+                                Statement.RETURN_GENERATED_KEYS);
+                System.out.println("ID REGISTRO = " + p.getId());
+                //insert Caso o ID seja null, então passo um randomUUID() para o novo contato. 
+                //Operador ternário aqui é uma ótima alternativa.
+                ps.setString(1, UUID.randomUUID().toString());//Id somente no insert
+                ps.setString(2, p.getNome());
+                ps.setString(3, p.getSobrenome());
+                ps.setString(4, p.getApelido());
+                ps.setDate(5, (p.getAniversario() != null ? new java.sql.Date(p.getAniversario().getTime()) : null));
+                ps.setString(6, p.getEndereco());
+            } else { // UPDATE
+                ps = conexao.connect()
+                        .prepareStatement(sqlUpdate,
+                                Statement.RETURN_GENERATED_KEYS);
+                System.out.println("ID REGISTRO = " + p.getId());
+                //insert Caso o ID seja null, então passo um randomUUID() para o novo contato. 
+                //Operador ternário aqui é uma ótima alternativa.
+                 ps.setString(1, p.getNome());
+                ps.setString(2, p.getSobrenome());
+                ps.setString(3, p.getApelido());
+                ps.setDate(4, (p.getAniversario() != null ? new java.sql.Date(p.getAniversario().getTime()) : null));
+                ps.setString(5, p.getEndereco());
+                ps.setString(6, p.getId());//Id somente no insert
+            }
+
+            System.out.println("SQL= " + ps.toString());
+            int result = ps.executeUpdate();
+            System.out.println("RESULT EXEC SQL= " + result);
+            pkReturn = p.getId();
+        } catch (SQLException ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+        return pkReturn;
+    }
+
 }
